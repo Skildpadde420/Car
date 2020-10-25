aa2430f72336607918e0546b1de0442cb5e84df9
Skildpadde420 <Skildpadde420@github.com>
2020-10-25 21:41:23 +0100

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    class Car
+    {
+        #region Member
+        #endregion
+
+        #region Constructor
+        //Standard Constructor
+        public Car()
+        {
+        }
+        public Car(string brand, string model, int horsePower)
+        {
+            Brand = brand;
+            Model = model;
+            HorsePower = horsePower;
+        }
+        #endregion
+
+        #region Properties
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int HorsePower { get; set; }
+        public string Color { get; set; }
+        #endregion
+
+        #region Function
+        public virtual void OutputCar()
+        {
+            if (String.IsNullOrEmpty(Color))
+                Console.WriteLine(string.Format("Brand: {1,15}{0}Model: {2,15}{0}HorsePower: {3,7} PS{0}",Environment.NewLine, Brand, Model, HorsePower));
+            else
+                Console.WriteLine(string.Format("Brand: {1,15}{0}Model: {2,15}{0}HorsePower: {3,7} PS{0}Color: {4,15}", Environment.NewLine, Brand, Model, HorsePower, Color));
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate 2 new objects
+            Car car1 = new Car("Audi", "A3", 125);
+            Car car2 = new Car("VW", "Golf", 170);
+
+            car2.Color = "Red";
+
+            car1.OutputCar();
+            car2.OutputCar();
+
+            Console.ReadKey();
+        }
+    }
+}
